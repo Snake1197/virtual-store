c723a3ca59d2f791a90ea750fe045c5cf7c0d6be
Snake1197 <leandromartin747@gmail.com>
2024-06-03 15:54:36 -0500

    Renderizado correcto de imágenes de producto details

--- src/components/ImagesProductDetails.tsx
+++ src/components/ImagesProductDetails.tsx
@@ -1,6 +1,8 @@
 import styles from "./ImagesProductDetails.module.css";
+import { useState, useEffect } from "react";
 
 interface Product {
+	title: string;
   images: string[];
 }
 
@@ -9,18 +11,26 @@ interface ImagesProductDetailsProp{
 }
 
 function ImagesProductDetails({ product }: ImagesProductDetailsProp) {
+	//uso de State para renderizar las fotos mini cuando cambie de producto
+	const [thumb, setThumb] = useState(product.images[0] || "/mock1.jpg");
+	
+	//uso de Effect para renderizar la foto grande cuando cambie de producto
+	useEffect(() => {
+    setThumb(product.images[0] || "/mock1.jpg");
+  }, [product]);
   return (
     <>
       <section className={styles["product-images-block"]}>
         <div className={styles["product-images"]}>
           {product.images.map(
             (each, index) =>
-              index > 0 && (
+              (
                 <img
                   key={index}
                   className={styles["mini-img"]}
                   src={each}
-                  alt="MacBook Pro 13'4"
+                  alt={product.title}
+									onClick={() => setThumb(each)}
                 />
               )
           )}
@@ -28,8 +38,8 @@ function ImagesProductDetails({ product }: ImagesProductDetailsProp) {
         <img
           className={styles["big-img"]}
           id="big-img"
-          src={product.images[0]}
-          alt="MacBook Pro 13'4"
+          src={thumb}
+          alt={product.title}
         />
       </section>
     </>
